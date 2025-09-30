4d766029780b40aee619e19ce80e08973313abc3
yashkoparde <yashkoparde2022@gmail.com>
2025-09-30 07:28:26 +0530

    Update Leetcode Q3-Python.py

--- Leetcode Q3-Python.py
+++ Leetcode Q3-Python.py
@@ -1,16 +1,16 @@
-int lengthOfLongestSubstring(string s) {
-    unordered_map<char, int> seen; // stores last index of characters
-    int left = 0, maxLen = 0;
-
-    for (int right = 0; right < s.size(); right++) {
-        char c = s[right];
-        if (seen.count(c) && seen[c] >= left) {
-            // move left pointer right after last duplicate
-            left = seen[c] + 1;
-        }
-        seen[c] = right; // update last index
-        maxLen = max(maxLen, right - left + 1);
-    }
-
-    return maxLen;
-}
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        seen = {}
+        left = 0
+        max_len = 0
+    
+        for right, char in enumerate(s):
+        # If character is already seen and is inside the current window
+            if char in seen and seen[char] >= left:
+                left = seen[char] + 1  # move left pointer right after the duplicate
+        
+            seen[char] = right  # update last seen index of char
+            max_len = max(max_len, right - left + 1)
+    
+        return max_len
+        
