cad916d3f88eb94943afaf520b52995254f1a96e
yashkoparde <yashkoparde2022@gmail.com>
2025-10-19 17:30:12 +0530

    Create Q11.cpp

---
+++ Q11.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int l = 0, r = h.size() - 1, ans = 0;
+        while (l < r) {
+            ans = max(ans, min(h[l], h[r]) * (r - l));
+            if (h[l] < h[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
