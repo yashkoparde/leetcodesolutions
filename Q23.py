e9a8a766daf39240e33cfd32bc1bdbb380004e39
YASH B KOPARDE <yashkoparde2022@gmail.com>
2025-11-20 18:59:37 +0530

    Create Q23.py

---
+++ Q23.py
@@ -0,0 +1,23 @@
+#Q23.py
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists):
+        heap = []
+        
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+        
+        dummy = ListNode(0)
+        tail = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            tail.next = node
+            tail = tail.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
