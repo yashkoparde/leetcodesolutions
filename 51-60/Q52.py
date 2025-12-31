aa55a47a987ec0494dcae8d491c307a6e7584969
YASH B KOPARDE <yashkoparde2022@gmail.com>
2025-12-31 19:24:47 +0530

    Update Q52.py

--- 51-60/Q52.py
+++ 51-60/Q52.py
@@ -1,14 +1,15 @@
 #Q52.py
 class Solution:
     def totalNQueens(self, n):
-        res=0
-        def dfs(r,c,d1,d2):
-            nonlocal res
-            if r==n:
-                res+=1; return
-            ok=((1<<n)-1)&~(c|d1|d2)
+        res = [0]
+        def dfs(r, c, d1, d2):
+            if r == n:
+                res[0] += 1
+                return
+            ok = ((1 << n) - 1) & ~(c | d1 | d2)
             while ok:
-                p=ok&-ok; ok-=p
-                dfs(r+1, c|p, (d1|p)<<1, (d2|p)>>1)
-        dfs(0,0,0,0)
-        return res
+                p = ok & -ok
+                ok -= p
+                dfs(r + 1, c | p, (d1 | p) << 1, (d2 | p) >> 1)
+        dfs(0, 0, 0, 0)
+        return res[0]
